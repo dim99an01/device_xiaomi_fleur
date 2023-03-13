70788e625a8f06d3d7f7b1bcabd7d58a436a6d68
Sergeant Kakashi <111608769+mlzdbz@users.noreply.github.com>
2023-03-14 00:51:04 +0100

    fleur: import rootdirs stuff from stock

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #
 # Copyright (C) 2023 The LineageOS Project
 #
@@ -30,3 +31,15 @@ LOCAL_SRC_FILES    := etc/fstab.mt6781
 LOCAL_MODULE_PATH  := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_EXTRA_VNDK_VERSIONS)/etc
 include $(BUILD_PREBUILT)
 
+=======
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fstab.mt6781_ramdisk
+LOCAL_MODULE_STEM := fstab.mt6781
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := etc/fstab.mt6781
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/first_stage_ramdisk
+include $(BUILD_PREBUILT)
+>>>>>>> e2200b6... fleur: import rootdirs stuff from stock
