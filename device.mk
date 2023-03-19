22e21b9aeaed4eb5c3c3ad93e179f30380799133
Nem1xx <nemanjanedeljkovic738@gmail.com>
2023-03-19 16:09:28 +0000

    fleur: Drop sensors config

--- device.mk
+++ device.mk
@@ -143,10 +143,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.mt6781.rc:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init/hw/init.mt6781.rc \
     $(LOCAL_PATH)/rootdir/etc/init.mediatek.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.mediatek.rc
 
-# Sensors
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
 # Symbols
 PRODUCT_PACKAGES += \
     libshim_vtservice \
